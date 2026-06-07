e1c5f4d974ac598ee17485351cce2d8d179c8ba2
chuvavoi <nguyenhoangphu7a@gmail.com>
2026-06-07 17:26:25 +0700

    3rd

---
+++ src/lib/tanstack-start-injected-head-scripts.ts
@@ -0,0 +1,5 @@
+export function injectedHeadScripts() {
+  return [];
+}
+
+export default injectedHeadScripts;
---
+++ src/lib/tanstack-start-plugin-adapters.ts
@@ -0,0 +1,7 @@
+export const hasPluginAdapters = false;
+export const pluginSerializationAdapters = [];
+
+export default {
+  hasPluginAdapters,
+  pluginSerializationAdapters,
+};
